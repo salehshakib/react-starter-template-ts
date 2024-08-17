634ef9fd1b147b7f4a189ad1da65849f26939b35
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-08-17 12:48:06 +0600

    update package json and prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,18 @@
 module.exports = {
-    trailingComma: "es5",
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-  };
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  importOrder: [
+    '<BUILTIN_MODULES>',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '^[./]',
+    '',
+    '<TYPES>',
+    '<TYPES>^[./]',
+  ],
+  // plugins: ['prettier-plugin-sort-json', '@ianvs/prettier-plugin-sort-imports'],
+  printWidth: 120,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 import './App.css';
 
 const id = 123;
 
-function App() {
+const hello = { 1: 'hello', 2: 'world' };
+
+function App(): JSX.Element {
+  const [counter, setCounter] = useState(0);
+
   return <div className="App">hello world</div>;
 }
 
