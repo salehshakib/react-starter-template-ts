8a9fe2311dec4b72c49aa423a6039f8e59320b17
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-08-17 13:04:10 +0600

    update

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,16 +3,7 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  importOrder: [
-    '<BUILTIN_MODULES>',
-    '<THIRD_PARTY_MODULES>',
-    '',
-    '^@/(.*)$',
-    '^[./]',
-    '',
-    '<TYPES>',
-    '<TYPES>^[./]',
-  ],
+  importOrder: ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '', '^@/(.*)$', '^[./]', '', '<TYPES>', '<TYPES>^[./]'],
   // plugins: ['prettier-plugin-sort-json', '@ianvs/prettier-plugin-sort-imports'],
   printWidth: 120,
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-
 import './App.css';
+import Button from './components/Button/Button';
 
 const id = 123;
 
@@ -9,7 +9,11 @@ const hello = { 1: 'hello', 2: 'world' };
 function App(): JSX.Element {
   const [counter, setCounter] = useState(0);
 
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
 }
 
 export default App;
